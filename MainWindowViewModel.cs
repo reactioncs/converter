0584c5057f7d8c734707242d16ac11bf11921895
zzz <zzz@zzz.com>
2023-06-05 15:45:30 +0800

    Add OpenFileDialog & change project to nullable

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace ImageConverter
 {
@@ -22,20 +23,14 @@ namespace ImageConverter
 
         public ObservableCollection<LogItem> LogItems { get; set; }
 
-        public string FilePath { get; set; }
-        public string FileName { get; set; }
-
-        public RelayCommand SaveAsWEBPCommand { get; set; }
-        public RelayCommand SaveAsPNGCommand { get; set; }
-        public RelayCommand SaveAsAVIFCommand { get; set; }
-        public RelayCommand SaveAsHEIFCommand { get; set; }
-        public RelayCommand TestCommand { get; set; }
-        public RelayCommand WriteTestCommand { get; set; }
+        public RelayCommand OpenFolderCommand { get; set; }
+        public RelayCommand Test0Command { get; set; }
+        public RelayCommand Test1Command { get; set; }
         public RelayCommand ClearCommand { get; set; }
 
         public DateTime AddMessage(string message, DateTime? timePrevious = null)
         {
-            LogItem log = new LogItem()
+            LogItem log = new()
             {
                 Message = message,
                 TimePrevious = timePrevious ?? DateTime.Now
@@ -48,34 +43,81 @@ namespace ImageConverter
 
         public MainWindowViewModel()
         {
-            FilePath = "C:/Users/zzz/Desktop";
-            FileName = "test";
             LogItems = new ObservableCollection<LogItem>();
 
-            SaveAsWEBPCommand = new RelayCommand(o => Task.Run(() => {
-                var b = ImageGenerator.GenerateTestImage_8();
-                var b_ = ImageHelper.ConvertToWebpFormat(b, 1024, 1024, 1, 101);
-                File.WriteAllBytes("C:/Users/Dell/Desktop/tt/ori.webp", b_);
-                AddMessage("ori.webp");
-            }));
-            SaveAsPNGCommand = new RelayCommand(o => Task.Run(() => {
-            }));
-            SaveAsAVIFCommand = new RelayCommand(o => Task.Run(() => {
-            }));
-            SaveAsHEIFCommand = new RelayCommand(o => Task.Run(() => {
-            }));
-            TestCommand = new RelayCommand(o => Task.Run(() =>
+            Test0Command = new RelayCommand(o => Task.Run(() => DoTest0()));
+            Test1Command = new RelayCommand(o => Task.Run(() => SaveGenerateImageAsWebp()));
+
+            OpenFolderCommand = new RelayCommand(o =>
             {
-                var img = ImageHelper.ReadWebpFile("C:/Users/Dell/Desktop/tt/ori.webp");
-                var b_ = ImageHelper.ConvertToPngFormat(img.Bytes, img.Width, img.Height, 1);
-                File.WriteAllBytes("C:/Users/Dell/Desktop/tt/res.png", b_);
-                AddMessage("res.webp");
-            }));
-            WriteTestCommand = new RelayCommand(o =>
-            {
-                int c = Environment.ProcessorCount / 5;
+                string fileName = DoOpenFileDialog() ?? "null";
+                AddMessage(fileName);
             });
+
             ClearCommand = new RelayCommand(o => LogItems.Clear());
         }
+        private void DoTest0()
+        {
+        }
+
+        private static string? DoOpenFileDialog(string title = "saving", string initialDirectory = "C:\\")
+        {
+            OpenFileDialog fileDialog = new()
+            {
+                InitialDirectory = initialDirectory,
+                Filter = "Png File|*.png|Wepb File|*.webp|Avif File|*.avif|Heif File|*.heif",
+                Title = title
+            };
+
+            if (fileDialog.ShowDialog() == true)
+                return fileDialog.FileName;
+
+            return null;
+        }
+
+        private static string? DoSaveFileDialog(Format f, string title = "saving", string initialDirectory = "C:\\")
+        {
+            string filter = "";
+            filter += f.HasFlag(Format.Png) ? "Png File|*.png|" : "";
+            filter += f.HasFlag(Format.Wepb) ? "Wepb File|*.webp|" : "";
+            filter += f.HasFlag(Format.Avif) ? "Avif File|*.avif|" : "";
+            filter += f.HasFlag(Format.Heif) ? "Heif File|*.heif|" : "";
+
+            SaveFileDialog fileDialog = new()
+            {
+                InitialDirectory = initialDirectory,
+                // get rid of the last '|'
+                Filter = filter[..^1],
+                Title = title
+            };
+
+            if (fileDialog.ShowDialog() == true)
+                return fileDialog.FileName;
+
+            return null;
+        }
+
+        private void SaveGenerateImageAsWebp()
+        {
+            var b = ImageGenerator.GenerateTestImage_8();
+            var b_ = ImageHelper.ConvertToWebpFormat(b, 1024, 1024, 1, 101);
+
+            string? path = DoSaveFileDialog(Format.Wepb, title: "Save GenerateImage As Webp");
+            if (path == null)
+                return;
+
+            File.WriteAllBytes(path, b_);
+            AddMessage(path);
+        }
+    }
+
+    [Flags]
+    public enum Format
+    {
+        None = 0,
+        Png = 1,
+        Wepb = 2,
+        Avif = 4,
+        Heif = 8
     }
 }
--- Model/LogItem.cs
+++ Model/LogItem.cs
@@ -4,19 +4,13 @@ namespace ImageConverter.Model
 {
     public class LogItem
     {
-        public DateTime Time { get; set; }
+        public DateTime Time { get; set; } = DateTime.Now;
         public DateTime TimePrevious { get; set; }
-        public string Message { get; set; }
+        public string Message { get; set; } = "";
 
         public int Span
         {
             get => (int)new TimeSpan(Time.Ticks - TimePrevious.Ticks).TotalMilliseconds;
-        }
-
-        public LogItem()
-        {
-            Time = DateTime.Now;
-            Message = "";
         }
     }
 }
