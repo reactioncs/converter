407d5d5129d4f30e874b5dcd5b4bc9db632074d9
zzz <zzz@zzz.com>
2023-06-28 09:17:49 +0800

    Implement ClearAllCommand

--- UI.Logging/LoggingViewModel.cs
+++ UI.Logging/LoggingViewModel.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Services.Logging;
 
 namespace UI.Logging
 {
-    public class LoggingViewModel
+    public partial class LoggingViewModel: ObservableObject
     {
         public ObservableCollection<LogItem> LogItemCollection { get; set; } = new();
 
         public LoggingViewModel()
         {
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test0"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test1"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test2"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
-            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Test3"));
+            LogItemCollection.CollectionChanged += (o, e) => ClearAllCommand.NotifyCanExecuteChanged();
+
+            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Platform and Runtime Independent"));
+            LogItemCollection.Add(new(DateTime.Now, "LoggingViewModel", "Simple to pick-up and use\n Reference Implementation"));
+        }
+
+        [RelayCommand(CanExecute = nameof(IsClearAllCanExecute))]
+        private void ClearAll()
+        {
+            LogItemCollection.Clear();
+        }
+        private bool IsClearAllCanExecute() => LogItemCollection.Count > 0;
+
+        [RelayCommand]
+        private void RemoveSelected()
+        {
+            throw new NotImplementedException();
         }
     }
 }
